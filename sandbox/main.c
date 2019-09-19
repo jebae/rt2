300c9afdd9dd5f864a773d6bafda2a49a4fd894e
Madeleine HERNANDEZ <mhernand@student.42.fr>
2019-09-19 12:57:10 +0200

    modifying cyn

--- sandbox/main.c
+++ sandbox/main.c
@@ -36,17 +36,15 @@ int	main(int ac, char **av)
 	t_ol pla; pla.cur_shape = 4;
 	pla.nor.x = 5; pla.nor.y = 1; pla.nor.z = 3; pla.d = 2;
 	pla.next = NULL; pla.radius = 50;
-	// t_ll	*ll = NULL;
 	t_ol	*ol_pla;
 	ol_pla = &pla;
 	/////////////////////////////////////////////////////
 
 	///////////////// C Y L I N D E R ///////////////////
 	t_ol cl; cl.cur_shape = 3;
-	cl.dir.x = 1; cl.dir.y = 0; cl.dir.z = 3; 
-	cl.cen.x = 5; cl.cen.y = 1; cl.cen.z = 3; 	
+	cl.dir.x = 0; cl.dir.y = 3; cl.dir.z = 5; 
+	cl.cen.x = 15; cl.cen.y = 0; cl.cen.z = 0;
 	cl.next = NULL; cl.radius = 50;
-	// t_ll	*ll = NULL;
 	t_ol	*ol_cl;
 	ol_cl = &cl;
 	/////////////////////////////////////////////////////
@@ -55,8 +53,7 @@ int	main(int ac, char **av)
 	t_ol co; co.cur_shape = 2;
 	co.dir.x = 10; co.dir.y = 10; co.dir.z = 0; 
 	co.cen.x = 0; co.cen.y = 0; co.cen.z = 10; 	
-	co.next = NULL; co.radius = 20; co.angle = M_PI / 6;
-	// t_ll	*ll = NULL;
+	co.next = NULL; co.radius = 5; co.angle = M_PI / 6;
 	t_ol	*ol_co;
 	ol_co = &co;
 	/////////////////////////////////////////////////////
@@ -64,9 +61,10 @@ int	main(int ac, char **av)
 	e = &env;
 	e->cam.campos.x = 0; e->cam.campos.y = 0; e->cam.campos.z = -50;
 	(e->w) = init(&(e->w));
-//	sand2(e, ol_pla, ll);
-//	sand2(e, ol, ll);
-	sand2(e, ol_co, ll);
+	// sand2(e, ol_pla, ll);
+	// sand2(e, ol, ll);
+	// sand2(e, ol_co, ll);
+	sand2(e, ol_cl, ll);
 	mlx_key_hook(e->w.wp, key_hook, e);
 	mlx_loop(e->w.mp);
 	return (0);
