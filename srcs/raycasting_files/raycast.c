85fcc93d89ae6821c5228fe53fa5227cc2b97c90
Samuel BONIFACE <sabonifa@student.42.fr>
2019-10-02 12:04:26 +0200

    Makefile working

--- srcs/raycasting_files/raycast.c
+++ srcs/raycasting_files/raycast.c
@@ -6,24 +6,11 @@
 /*   By: sabonifa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 17:15:40 by sabonifa          #+#    #+#             */
-/*   Updated: 2019/09/26 14:23:29 by sabonifa         ###   ########.fr       */
+/*   Updated: 2019/10/02 12:01:16 by sabonifa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raycast.h"
-
-double	intersection(t_vec3 ray, t_ol *ol, t_env *e)
-{
-	if (ol->cur_shape == 1)
-		return (v_intersect_sp(ray, ol, e));
-//	if (ol->cur_shape == 2)
-//		return (v_intersect_co(ray, ol, e));
-//	if (ol->cur_shape == 3)
-//		return (v_intersect_cy(ray, ol, e));
-	if (ol->cur_shape == 4)
-		return (v_intersect_pl(ray, ol, e));
-	return (0);
-}
 
 double  intersection2(t_ray ray, t_ol *ol, t_env *e)
 {
@@ -33,6 +20,8 @@ double  intersection2(t_ray ray, t_ol *ol, t_env *e)
 		return (v_intersect_co(ray, ol, e));
 	if (ol->cur_shape == 3)
 		return (v_intersect_cy(ray, ol, e));
+	if (ol->cur_shape == 4)
+		return (v_intersect_pl(ray, ol, e));
 	return (0);
 }
 
@@ -59,18 +48,16 @@ t_ray   cast_ray(int x, int y, t_camera cam)
 	return (ray);
 }
 
-int 			raycast(t_env *e, t_ol *ol, t_ll *ll)
+int raycast(t_env *e, t_ol *ol, t_ll *ll)
 {
-	int 		x;
-	int 		y;
-	t_ray   	ray;
-	t_ll    	*tp_l;
-	t_ol    	*tp_o;
+	int x = 0;
+	int y = 0;
+	t_ray   ray;
+	t_ll    *tp_l;
+	t_ol    *tp_o;
 	t_shader	sh;
-	double		r;
 
-	x = 0;
-	y = 0;
+	double r;
 	while (x < WIDTH)
 	{
 		y = 0;
