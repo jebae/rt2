53252aa9308c2b61defab0f2efc7a48ef3abc5bb
Samuel BONIFACE <sabonifa@student.42.fr>
2019-11-05 12:47:41 +0100

    Push from my gitlab of new scenes and object manipulation for trans and rot

---
+++ srcs/raycasting_files/object_manipulation.c
@@ -0,0 +1,22 @@
+
+
+#include "raycast.h"
+
+t_point translation(t_point pos, t_vec3 trans)
+{
+    t_point new;
+    new.x = pos.x + trans.x;
+    new.y = pos.y + trans.y;
+    new.z = pos.z + trans.z;
+    return (new);
+}
+
+t_vec3  rotation(t_vec3 axis, t_vec3 rotx, t_vec3 roty, t_vec3 rotz)
+{
+    t_vec3  new;
+
+    new.x = rotx.x * axis.x + roty.x * axis.y + rotz.x * axis.z ;
+    new.x = rotx.y * axis.x + roty.y * axis.y + rotz.y * axis.z ;
+    new.x = rotx.z * axis.x + roty.z * axis.y + rotz.z * axis.z ;
+    return (new);
+}
