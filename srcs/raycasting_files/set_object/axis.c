c7b39a950ce6ae6e03e967fb40847553bd410488
Jeongmo BAE <jebae@e1r12p16.42.fr>
2019-12-04 22:05:32 +0100

    modify pick_axis_from_plane function to normalise x

--- srcs/raycasting_files/set_object/axis.c
+++ srcs/raycasting_files/set_object/axis.c
@@ -10,6 +10,7 @@ void		pick_axis_from_plane(t_vec3 *normal, t_mat3 *axis_mat)
 	x = v3_cross(*normal, xw);
 	if (v3_norm2(x) < APPROX_0)
 		x = v3_cross(*normal, yw);
+	x = v3_normalise(x);
 	z = v3_cross(x, *normal);
 	ft_memcpy(axis_mat->arr[0], &x, sizeof(t_vec3));
 	ft_memcpy(axis_mat->arr[1], normal, sizeof(t_vec3));
